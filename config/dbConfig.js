0480e53704381e043493e32b19d7155a52d0ec1d
MartinGonzalez100 <martineg100@gmail.com>
2021-02-11 18:53:31 -0300

    actualizacion de valores de base de datos

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,12 +2,20 @@ const mysql = require('mysql')
 
 
 
+//base original
+// var conexion_db = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : '',
+//     database : 'baseproyecto'
+//   });
 
+//base heroku
 var conexion_db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'baseproyecto'
+    host     : 'us-cdbr-east-03.cleardb.com',
+    user     : 'b93c7478471bd9',
+    password : '0a9840ad',
+    database : 'heroku_25749191c95c6d8'
   });
   
   conexion_db.connect(err =>{
